3661d8e3465ea6bd95df486b4f85932c8a4bca46
kingham <1425962791@qq.com>
2026-05-26 18:29:01 +0800

    删除宏定义

--- 2.software/2.Advanced/6.LVGL-SmartWatch/OV_Watch/User/GUI_App/ui.c
+++ 2.software/2.Advanced/6.LVGL-SmartWatch/OV_Watch/User/GUI_App/ui.c
@@ -6,11 +6,9 @@
 #include "ui_helpers.h"
 
 ///////////////////// TEST LVGL SETTINGS ////////////////////
+
 #if LV_COLOR_DEPTH != 16
     #error "LV_COLOR_DEPTH should be 16bit to match SquareLine Studio's settings"
-#endif
-#if LV_COLOR_16_SWAP !=0
-    #error "LV_COLOR_16_SWAP should be 0 to match SquareLine Studio's settings"
 #endif
 
 /////////////////////// Timer //////////////////////
